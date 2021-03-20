edbe75a4dd85895ed9e8532eba26c92e866ba885
bratburn <73037441+bratburn@users.noreply.github.com>
2021-03-20 14:42:55 +0100

    8-9_class texbook
    exercises

---
+++ z3.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+
+int circle_inside (float r, float x, float y, float wx, float wy)
+{if ((wx-x) * (wx-x) + (wy-y) * (wy-y) > (r *r))
+ {return 0;}
+ return 1;
+}
+
+int main()
+{
+    float x, y, r;
+    int wx, wy, sum=0;
+    puts ("введите координаты центра окружности:");
+    scanf ("%f ; %f", &x, &y);
+    puts ("введите значение радиуса:");
+    scanf ("%f", &r);
+    for (wx = x-r; wx <= x+r; wx++)
+    {
+        for(wy = y-r; wy <= y+r; wy++)
+        {
+            if(circle_inside (x,y,r,wx,wy))
+            sum++;
+        }
+        
+    }
+    
+    printf ("Всего %i целочисленных координат", sum);
+    
+}
---
+++ z33.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+struct _circle { float x, y, r;};
+typedef struct _circle circle;
+
+int circle_inside (circle c, float wx, float wy)
+{if ((wx-c.x) * (wx-c.x) + (wy-c.y) * (wy-c.y) > (c.r*c.r))
+ {return 0;}
+ return 1;
+}
+
+int main()
+{
+    circle c;
+    int wx, wy, sum=0;
+    puts ("введите координаты центра окружности:");
+    scanf ("%f ; %f", &c.x, &c.y);
+    puts ("введите значение радиуса:");
+    scanf ("%f", &c.r);
+    for (wx = c.x-c.r; wx <= c.x+c.r; wx++)
+    {
+        for(wy = c.y-c.r; wy <= c.y+c.r; wy++)
+        {
+            if(circle_inside (c,wx,wy))
+            sum++
+        }
+        
+    }
+    
+    printf ("Всего %i целочисленных координат", sum);
+    
+}
